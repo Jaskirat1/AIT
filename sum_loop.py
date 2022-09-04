ee8d472c15f88e7fa7c70c7024006b3d43f12d81
Jaskirat Kaur <51392181+Jaskirat1@users.noreply.github.com>
2022-09-05 00:40:28 +0530

    Assignment 2
    Sum of elements in a list

---
+++ sum_loop.py
@@ -0,0 +1,16 @@
+from re import X
+
+
+num = int(input("Enter a number:-"))
+print("List of all numbers is :-")
+i = 0
+list=[]
+while i in range(0,num):
+    i += 1
+    list.append(i)
+    r = sum(list)
+print(list)
+print("sum is:-", r)
+
+    
+    
